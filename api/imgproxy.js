655edcc13b305d47295ec918ec831d3adc80d2ed
dimaszzz-benci <dimaszzz1601@gmail.com>
2026-03-15 00:30:30 +0700

    imgproxy.js

--- api/imgproxy.js
+++ api/imgproxy.js
@@ -1,1 +1,14 @@
-
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).send("No URL");
+  try {
+    const response = await fetch(decodeURIComponent(url));
+    const buffer = await response.arrayBuffer();
+    const contentType = response.headers.get("content-type") || "image/jpeg";
+    res.setHeader("Content-Type", contentType);
+    res.setHeader("Cache-Control", "public, max-age=86400");
+    res.send(Buffer.from(buffer));
+  } catch {
+    res.status(500).send("Error");
+  }
+}
